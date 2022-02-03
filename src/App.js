20eeacec45ad59914ae4b39301fca3950593ecda
Gabriel Vacari <gabriel_vacari@outlook.com>
2022-02-03 13:53:03 -0300

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'Car insurance', amount: 294.98, date: new Date(2021, 2, 10) },
+    { id: 'e2', title: 'Car insurance', amount: 294.98, date: new Date(2021, 2, 10) },
+    { id: 'e3', title: 'Car insurance', amount: 294.98, date: new Date(2021, 2, 10) },
+    { id: 'e4', title: 'Car insurance', amount: 294.98, date: new Date(2021, 2, 10) },
+  ]
+
   return (
     <div className="App">
-      <h2>Hello World!</h2>
+      <ExpenseItem 
+        title={expenses[0].title} 
+        amount={expenses[0].amount}
+        date={expenses[0].date}  
+      />
+      <ExpenseItem 
+        title={expenses[1].title} 
+        amount={expenses[1].amount}
+        date={expenses[1].date}  
+      /> 
     </div>
   );
 }
